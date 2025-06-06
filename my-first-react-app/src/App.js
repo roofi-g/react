973058c95da266979b9180a2149f9f708f1d4cd3
Roofi <roofi.g@yandex.ru>
2025-06-06 21:06:15 +0500

    update app.js

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import CurrentTime from './components/currentTime/CurrentTime';
+import EventCard from './components/eventCard/EventCard';
+import Counter from "./components/Counter/Counter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greeting />
+      <CurrentTime />
+      <Counter />
+      {/*<EventCard name='Свадьба' date='13 май' location='Челябинск'/>*/}
+      {/*<EventCard name='Юбилей' date='17 июнь' location='Екатеринбург'/>*/}
+    </div>
+  );
+}
+
+function Greeting() {
+  const currentTime = new Date().getHours();
+  let currentTimeOfDay = 'Добрый утро!';
+  if (currentTime > 10 && currentTime < 18) currentTimeOfDay = 'Доброе день!';
+  if (currentTime > 18 && currentTime < 22) currentTimeOfDay = 'Добрый вечер!';
+  if (currentTime > 22 || currentTime < 3) currentTimeOfDay = 'Доброй ночи!';
+  return (
+    <div className="Greeting">
+      <h1>Привет, React!</h1>
+      <p> {currentTimeOfDay} </p>
     </div>
   );
 }
