4189be7ef8aaa8a4672ae6cf7ca1e0ec61462793
Roofi <roofi.g@yandex.ru>
2025-09-16 18:05:36 +0500

    small changes

--- my-first-react-app/src/components/MessagesList/MessagesList.js
+++ my-first-react-app/src/components/MessagesList/MessagesList.js
@@ -5,11 +5,11 @@ function MessagesList() {
   const [message, setMessage] = useState([
     {id: 1, text: 'Привет! Как дела?', sender: 'other'},
     {id: 2, text: 'Привет! Всё хорошо, как твои дела?', sender: 'me'},
-    {id: 3, text: 'Отлично!', sender: 'other'},
+    {id: 3, text: 'Отлично!', sender: 'other'}
   ]);
 
   return (
-    <Paper elevation={4} sx={{width: "50%", height: 200, bgcolor: "#f5f5f5", padding: "30px"}}>
+    <Paper elevation={4} sx={{height: 200, bgcolor: "#f5f5f5", padding: "30px"}}>
       <Stack spacing={2} sx={{flexGrow: 1}}>
         {message.map(message => (
           <Box
