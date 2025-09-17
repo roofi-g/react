324791b5348330c8b0c4e4e85c8bdf8db0c75209
Roofi <roofi.g@yandex.ru>
2025-09-17 23:10:33 +0500

    divided into components. add styles material UI

--- my-first-react-app/src/components/TodoList/TodoList.js
+++ my-first-react-app/src/components/TodoList/TodoList.js
@@ -1,38 +1,68 @@
 import './TodoList.css'
+import {Button, Box, TextField, ListItem, IconButton} from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
 const {useState} = require("react");
 
-function TodoList() {
+function TodoInput({ onAdd }) {
   const [value, setValue] = useState('');
+
+  const addElement = (e) => {
+    if (!value.trim()) return;
+    onAdd(value);
+    setValue('');
+  }
+
+  return (
+    <Box component="section">
+      <TextField
+        size="small"
+        label={"Сделать..."}
+        sx={{marginRight: '10px'}}
+        value={value}
+        onChange={(event) => setValue(event.target.value)}/>
+      <Button variant="outlined" onClick={addElement}>Добавить</Button>
+    </Box>
+  );
+}
+
+function TodoItem({item, onDelete}) {
+  return (
+    <ListItem>
+      {item.id}. {item.text}
+      <IconButton
+        aria-label="delete"
+        onClick={() => onDelete(item)}
+      >
+        <DeleteIcon />
+      </IconButton>
+    </ListItem>
+  );
+}
+
+function TodoList({ items, onDelete }) {
+  return (
+    <>
+      {items.map(el => <TodoItem key={el.id} item={el} onDelete={onDelete} />)}
+    </>
+  );
+}
+
+export default function ToDo() {
   const [items, setItems] = useState([
     {id: 1, text: 'Новая заметка'}
   ]);
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  }
-
-  const addElementInList = (e) => {
-    e.preventDefault();
-    if (!value.trim()) return;
+  const addElementInList = (value) => {
     setItems([...items, {id: items.length + 1, text: value}]);
-    setValue('')
   }
-
-  function deleteElementFromList(element) {
+  const deleteElementFromList = (element) => {
     setItems(prevItems => prevItems.filter(el => el.id !== element.id))
   }
 
   return(
     <div className={"todoList"}>
-      <form>
-        <input type="text" value={value} onChange={handleChange}/>
-        <button onClick={addElementInList}>Добавить</button>
-      </form>
-      <ul>
-        {items.map(el => <li key={el.id}>{el.id}. {el.text} <button onClick={() => deleteElementFromList(el)}>Удалить</button></li>)}
-      </ul>
+      <TodoInput onAdd={addElementInList} />
+      <TodoList items={items} onDelete={deleteElementFromList} />
     </div>
   );
 }
-
-export default TodoList;
