f764ec3578ebc53b0d063b0d7dd28d0f8e3b58a9
Roofi <roofi.g@yandex.ru>
2025-09-30 22:03:27 +0500

    added logic to the button

--- my-first-react-app/src/store/features/product/Product.js
+++ my-first-react-app/src/store/features/product/Product.js
@@ -1,10 +1,27 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useState} from "react";
+import {addProduct, deleteProduct} from "./favoritesSlice";
+
 export default function Product({ product }) {
+  const favorites = useSelector(state => state.favorites.favorites);
+  const dispatch = useDispatch();
+
+  const isFavorite = favorites.includes(product.id)
+
+  function click() {
+    if (!isFavorite) {
+      dispatch(addProduct(product));
+    } else {
+      dispatch(deleteProduct(product));
+    }
+  }
+
   return (
     <div>
       <h3>{product.name}</h3>
       <p>{product.description}</p>
       <p>Цена: {product.price} руб</p>
-      <button>Добавить в избранное</button>
+      <button onClick={click}>{ isFavorite ? 'Удалить из избранного' : 'Добавить в избранное' }</button>
     </div>
   )
 }
