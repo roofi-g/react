f6fbdfa1e165400ea30aa76a499bec71abbc7cd0
Roofi <roofi.g@yandex.ru>
2025-10-01 22:35:22 +0500

    moved the form to a separate component Form

--- my-first-react-app/src/store/features/product_admin/ProductCreate.js
+++ my-first-react-app/src/store/features/product_admin/ProductCreate.js
@@ -1,33 +1,14 @@
-import {useState} from "react";
 import {useDispatch} from "react-redux";
 import { addProduct } from "./productsSlice";
+import Form from "./Form";
 
 export default function ProductCreate() {
-  const [name, setName] = useState('Название');
-  const [description, setDescription] = useState('Описание');
-  const [price, setPrice] = useState('1000');
-  const [available, setAvailable] = useState('2');
-
   const dispatch = useDispatch();
 
-  const sendData = (e) => {
-    e.preventDefault();
-
-    if (!name.trim() || !description.trim() || !price.trim() || !available.trim()) return;
-
+  const sendData = (name, description, price, available) => {
     dispatch(addProduct({id: Date.now(), name, description, price, available}));
-
-    // setName('');
-    // setDescription('');
-    // setPrice('');
-    // setAvailable('');
   }
 
-  const style_div = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    margin: '10px 0',
-  }
   return (
     <div style={{
       width: '350px',
@@ -36,37 +17,7 @@ export default function ProductCreate() {
       padding: '20px'
     }}>
       <h2>Данные нового продукта</h2>
-      <form action="" onSubmit={sendData}>
-        <div style={style_div}>
-          <label>Название товара:</label>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}/>
-        </div>
-        <div style={style_div}>
-          <label>Описание товара:</label>
-          <input
-            type="text"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}/>
-        </div>
-        <div style={style_div}>
-          <label>Цена:</label>
-          <input
-            type="text"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}/>
-        </div>
-        <div style={style_div}>
-          <label>Количество:</label>
-          <input
-            type="text"
-            value={available}
-            onChange={(e) => setAvailable(e.target.value)}/>
-        </div>
-        <button type="submit">Добавить в каталог</button>
-      </form>
+      <Form onSendData={sendData} buttonLabel={'Добавить в каталог'}/>
     </div>
   )
 }
